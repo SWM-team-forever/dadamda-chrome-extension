0ce83b65ca1bbc62fea2eda36911f2c1512713c3
HanDaYeon-coder <hdy78901@naver.com>
2023-07-20 15:16:24 +0900

    [DAD-340] refactor: 팝업창 대신 content-script 넣는 구조로 변경

--- background/background.js
+++ background/background.js
@@ -1,52 +1,56 @@
-const POST_SCRAP_URL = "http://localhost:8080/api/v1/product";
+const POST_SCRAP_URL = "http://localhost:8080/v1/scraps";
 const TOKEN_URL = "http://localhost:8080/?token=";
 const GOOGLE_LOGIN_URL = "http://localhost:8080/oauth2/authorization/google";
 
 let newToken = null;
 
-function getToken() {
-  if (newToken === null) {
-    if (chrome.storage.local.get('signedIn')) {
-      chrome.storage.local.get(['accessToken'], (result) => {
-        newToken = result.accessToken;
-      });
-    }
-  }
-}
-
-async function postUrl(postScrapURL, currentUrl) {
-  fetch(postScrapURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-AUTH-TOKEN": newToken
-    },
-    body: JSON.stringify({
-      url: `${currentUrl}`,
-    }),
-  }).then((response) => {
-    console.log(response);
-    if (response.status >= 300) { //token 기한이 만료된 경우
-      chrome.storage.local.clear();
-      setPopupHTML();
-    }
+function contentScriptJS(tabId, file){
+  chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    files: [`${file}`]
   });
 }
 
-function scrap() {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const tab = tabs[0];
-    const currentUrl = tab.url;
-    postUrl(POST_SCRAP_URL, currentUrl);
+function contentScriptCSS(tabId, file){
+  chrome.scripting.insertCSS({
+    target: { tabId: tabId },
+    files: [`${file}`]
   })
 }
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message === "google-login") {
-    try {
-      chrome.tabs.create({ active: true, url: GOOGLE_LOGIN_URL });
+chrome.action.onClicked.addListener(async (tab) => {
+      chrome.storage.local.get('signedIn').then(async (result) => {
+        if (result.signedIn) {
+          contentScriptJS(tab.id, "content/content.js");
+          contentScriptCSS(tab.id, "content/content.css");
 
-      chrome.tabs.onUpdated.addListener(function authorizationHook(tabId, changeInfo, tab) {
+          await chrome.storage.local.get('accessToken').then((result) => {
+            newToken = result.accessToken;
+          });
+
+          const response = await postUrl(POST_SCRAP_URL, tab.url);
+
+          if (response === "Success") {
+            contentScriptJS(tab.id, "content/successContent.js");
+          } else if (response === "BR002") {
+            contentScriptJS(tab.id, "content/duplicatedScrap.js");
+          } else if (response === "NF002" || response === "BR001") {
+            googleLogin();
+          } else {
+            contentScriptJS(tab.id, "content/errorContent.js")
+          }
+        } else {
+          googleLogin()
+        }
+      })
+    }
+)
+
+function googleLogin() {
+  chrome.tabs.create({ active: true, url: GOOGLE_LOGIN_URL });
+
+  chrome.tabs.onUpdated.addListener(
+      function authorizationHook(tabId, changeInfo, tab) {
         if (tab.url.startsWith(TOKEN_URL)) {
 
           const tabURL = tab.url;
@@ -54,39 +58,35 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
           chrome.storage.local.set({ 'accessToken': token });
 
+          newToken = token;
+
           chrome.storage.local.set({ 'signedIn': true });
 
           chrome.tabs.onUpdated.removeListener(authorizationHook);
-
-          sendResponse("Success");
-
-          setPopupHTML();
-        } else {
-          sendResponse("Fail");
         }
-      });
-    } catch {
-      sendResponse("Fail");
-    } finally {
-      return true;
-    }
-  } else if (message === "save") {
-    try {
-      getToken();
-      scrap();
-      sendResponse("Success");
-    } catch {
-      sendResponse("Fail");
-    } finally {
-      return true;
-    }
-  }
-})
+      })
+}
 
-function setPopupHTML() {
-  if (chrome.storage.local.get('signedIn')) {
-    chrome.action.setPopup({ popup: '/popup/popup.html' });
-  } else {
-    chrome.action.setPopup({ popup: '/popup/popupSignIn.html' });
+async function postUrl(postScrapURL, currentUrl) {
+  let response = await fetch(postScrapURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-AUTH-TOKEN": newToken
+    },
+    body: JSON.stringify({
+      pageUrl: `${currentUrl}`
+    }),
+  });
+  console.log(response);
+
+  if (response.status >= 300) {
+    let responseJson = await response.json();
+    let errorCode = await responseJson.resultCode;
+    console.log(errorCode);
+    return errorCode;
+  }
+  else {
+    return "Success";
   }
 }
