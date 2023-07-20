4449ab4e0424ce1cbcd2da925293183570d88508
HanDaYeon-coder <hdy78901@naver.com>
2023-07-20 15:13:28 +0900

    [DAD-340] refactor: 사용하지 않는 팝업 관련 파일 제거

--- popup/popupSignIn.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    var y = document.getElementById("google-login-button");
-    y.addEventListener("click", openIndex);
-});
-
-function openIndex() {
-    chrome.runtime.sendMessage("google-login", (response) => { });
-}
