8ba4788c716d423894db97a8373667dbd70dc227
HanDaYeon-coder <hdy78901@naver.com>
2023-08-04 21:56:19 +0900

    [DAD-519] chore: api url 추가

---
+++ assets/api/apiUrl.js
@@ -0,0 +1,4 @@
+const POST_SCRAP_URL = "https://api.dadamda.me/v1/scraps";
+const TOKEN_URL = "https://dadamda.me/google-login?token=";
+const POST_HIGHLIGHTS_URL = "https://api.dadamda.me/v1/scraps/highlights";
+const LOGIN_URL = "https://dadamda-chrome-extention.s3.ap-northeast-2.amazonaws.com/src/login.html";
