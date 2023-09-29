46227d9f6f300a7128ead73d2c9e42edf518b120
HanDaYeon-coder <hdy78901@naver.com>
2023-09-29 18:56:31 +0900

    [DAD-881] refactor: 운영 서버 url로 변경

--- assets/api/apiUrl.js
+++ assets/api/apiUrl.js
@@ -1,20 +1,23 @@
-// const POST_SCRAP_URL = "https://api.dadamda.me/v1/scraps";
-// const TOKEN_URL = "https://dadamda.me/google-login?token=";
-// const POST_HIGHLIGHTS_URL = "https://api.dadamda.me/v1/scraps/highlights";
+const POST_SCRAP_URL = "https://api.dadamda.me/v1/scraps";
+const TOKEN_URL = "https://dadamda.me/oauth-login?token=";
+const POST_HIGHLIGHTS_URL = "https://api.dadamda.me/v1/scraps/highlights";
 
-// // 해당 URL 경로 변경시, Path: assets/login/login.html에 있는 <a> 태그(id = dadamda-google-login)의 src도 함께 바꿔줘야 함.
-// const GOOGLE_LOGIN_URL = "https://api.dadamda.me/oauth2/authorization/google";
+// 해당 URL 경로 변경시, Path: assets/login/login.html에 있는 <a> 태그(id = dadamda-google-login, dadamda-kakao-login)의 src도 함께 바꿔줘야 함.
+const GOOGLE_LOGIN_URL = "https://api.dadamda.me/oauth2/authorization/google";
+const KAKAO_LOGIN_URL = "https://api.dadamda.me/oauth2/authorization/kakao";
 
 // const POST_SCRAP_URL = "http://localhost:8080/v1/scraps";
-// const TOKEN_URL = "http://localhost:8080/google-login?token=";
+// const TOKEN_URL = "http://localhost:8080/oauth-login?token=";
 // const POST_HIGHLIGHTS_URL = "http://localhost:8080/v1/scraps/highlights";
 
-// // 해당 URL 경로 변경시, Path: assets/login/login.html에 있는 <a> 태그(id = dadamda-google-login)의 src도 함께 바꿔줘야 함.
+// // 해당 URL 경로 변경시, Path: assets/login/login.html에 있는 <a> 태그(id = dadamda-google-login, dadamda-kakao-login)의 src도 함께 바꿔줘야 함.
 // const GOOGLE_LOGIN_URL = "http://localhost:8080/oauth2/authorization/google";
+// const KAKAO_LOGIN_URL = "http://localhost:8080/oauth2/authorization/kakao";
 
-const POST_SCRAP_URL = "https://api-dev.dadamda.me/v1/scraps";
-const TOKEN_URL = "https://dev.dadamda.me/google-login?token=";
-const POST_HIGHLIGHTS_URL = "https://api-dev.dadamda.me/v1/scraps/highlights";
+// const POST_SCRAP_URL = "https://api-dev.dadamda.me/v1/scraps";
+// const TOKEN_URL = "https://dev.dadamda.me/oauth-login?token=";
+// const POST_HIGHLIGHTS_URL = "https://api-dev.dadamda.me/v1/scraps/highlights";
 
-// 해당 URL 경로 변경시, Path: assets/login/login.html에 있는 <a> 태그(id = dadamda-google-login)의 src도 함께 바꿔줘야 함.
-const GOOGLE_LOGIN_URL = "https://api-dev.dadamda.me/oauth2/authorization/google";
+// // 해당 URL 경로 변경시, Path: assets/login/login.html에 있는 <a> 태그(id = dadamda-google-login, dadamda-kakao-login)의 src도 함께 바꿔줘야 함.
+// const GOOGLE_LOGIN_URL = "https://api-dev.dadamda.me/oauth2/authorization/google";
+// const KAKAO_LOGIN_URL = "https://api-dev.dadamda.me/oauth2/authorization/kakao";
